c225c5f6199433983763dbd74e6fc80eaccfc27a
lucasstranger1 <lu.yao001@stu.bmcc.cuny.edu>
2023-10-15 18:18:42 -0400

    Initial commit

--- js/typewriter.js
+++ js/typewriter.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded',function(event){
     // array with texts to type in typewriter
-    var dataText = ["Python and Django Developer",
-    "Cyber Security",
-    "Cloud Computing",
+    var dataText = ["Web-programming With Python and Django Developer",
+    "Volleyball Player",
+    "Enthusiastic Leaner",
     "Open Source Contributor",];
     
     // type one text in the typwriter
